adf93964875af6fedfb04ae494f9057229679058
pc <alangerman98@yahoo.com.ar>
2025-07-04 11:58:08 -0300

    creacion de menu y submenu de cursos - corrección

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 
 import BottomNav from "./componets/BottomNav";
-import Curso from "./componets/Curso";
+
 import Navbar from "./componets/Navbar";
 
 export default function Home() {
@@ -9,8 +9,11 @@ export default function Home() {
     <div className="grid bg-violet-500 grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen bg-brand-primary  gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <Navbar titulo={"BIENVENIDO"} data={"INCIO"}></Navbar>
       
-        
-        <h3>Bienvenido</h3>
+<div>
+        <a href="/menu-cursos" className="text-violet-950 m-4 bg-amber-300 rounded-xl p-2">Ir a Cursos</a> <br />
+   
+</div>
+<a href="/menu-cursos" className="text-violet-950 m-4 bg-amber-300 rounded-xl p-2">Ver Alumnos</a>
 <BottomNav></BottomNav>
     </div>
   );
