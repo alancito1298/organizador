70b91f8e902c359d56c4e111f82bed9c4d24e34f
pc <alangerman98@yahoo.com.ar>
2026-02-05 23:14:54 -0300

    creacion de introduccion

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,7 +11,11 @@ export default function Home() {
       
 <div>
         <a href="/menu-cursos" className="font-bebas text-violet-950 m-4 bg-amber-300 rounded-xl p-2 flex justify-center align-center">Ir a Cursos</a> 
-        <p className="p-5 font-bebas">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Veritatis nemo, suscipit ab hic cum quod quas vero deserunt omnis repellat iusto molestiae iste nesciunt nam quae fugiat id provident itaque?</p>   
+        <p className="p-5 font-bebas">Esta web sirve para que los docentes puedan organizar y gestionar toda su actividad educativa en un solo lugar.
+
+Permite llevar un control ordenado de cursos, alumnos, asistencias, calificaciones y agenda, evitando el uso de cuadernos, planillas en papel o archivos dispersos. Cada docente accede con su cuenta y gestiona únicamente su propia información, de forma segura y desde cualquier dispositivo.
+
+El objetivo principal es simplificar la tarea diaria del docente, ahorrar tiempo administrativo y facilitar el seguimiento del trabajo pedagógico a lo largo del ciclo lectivo.</p>   
 </div>
 
 
