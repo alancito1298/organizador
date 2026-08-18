17365424fd97c26de1ece76c35802578d72824a8
pc <alangerman98@yahoo.com.ar>
2026-08-18 15:53:23 -0300

    SEO: agregar metadescripciones especificas a todas las rutas internas y sub-menus de cursos

--- src/app/sub-menu-curso/[id]/calificaciones/page.tsx
+++ src/app/sub-menu-curso/[id]/calificaciones/page.tsx
@@ -1,18 +1,18 @@
-'use client'
+import type { Metadata } from "next";
+import BottomNav from "../../../components/shared/BottomNav";
+import ListaCalificaciones from "../../../components/calificaciones/ListaCalficaciones";
 
-import React from "react"
+export const metadata: Metadata = {
+  title: "Calificaciones y Notas",
+  description: "Registro, carga y seguimiento de notas de exámenes, trabajos prácticos y promedios por trimestre.",
+  robots: { index: false, follow: true },
+};
 
-import BottomNav from "../../../components/shared/BottomNav"
-
-import ListaCalificaciones from "../../../components/calificaciones/ListaCalficaciones"
-
-
-export default function Calificaciones() {
+export default function CalificacionesPage() {
   return (
-    <div className=" bg-fuchsia-200 h-full">
-  
-     <ListaCalificaciones></ListaCalificaciones>
-     <BottomNav></BottomNav>
+    <div className="bg-fuchsia-200 h-full">
+      <ListaCalificaciones />
+      <BottomNav />
     </div>
   );
 }
